7ea8570d48f9d6fa2620412970cc88255499d4d4
stive <stiven23ospina@gmail.com>
2022-06-20 21:36:08 -0500

    Avance 1 todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  list:any[]=[];
+  addTask(item:string){
+    this.list.push({id:this.list.length, name:item})
+    console.warn(this.list);
+  }
+  removeTask(id:number){
+    console.warn(id);
+    this.list=this.list.filter(item=>item.id!==id);
+  }
 }
